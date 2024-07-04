fa702b952c9e748bb006b5a4e0761add9e9a8d11
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-04 17:05:48 +0100

    update coke.py

--- coke.py
+++ coke.py
@@ -3,8 +3,7 @@ due = 50
 print ("Amount Due: 50")
 
 while due > 0:
-    coins = int(input("Insert Coin: "))
-    
+    coins = int(input("Insert Coin: "))   
     if coins == 25 or coins == 10 or coins == 5:
         due = due - coins
         if due > 0:
@@ -13,7 +12,6 @@ while due > 0:
             print("Change Owed:", abs(due))
     else:
         print("Amount Due:", due)
-
 
         
        
