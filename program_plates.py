4eb20bb4fc1b2a684bd62c93967c34d2a699627c
Ningrui Zhang <ningrui1227@gmail.com>
2024-07-09 10:50:17 +0100

    add file

---
+++ program_plates.py
@@ -0,0 +1,65 @@
+import string
+
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+def is_valid(s):
+    # print("is_first_two_letter:", is_first_two_letter(s))
+    # print("is_length:", is_length(s))
+    # print("is_middle_letter", is_middle_letter(s))
+    # print("is_first_number", is_first_number(s))
+    # print("is_punctuation", is_punctuation(s))
+    if  (is_first_two_letter(s) and
+        is_length(s) and
+        is_middle_letter(s) and 
+        is_first_number(s) and
+        is_punctuation(s)):
+        return True
+    else:
+        return False
+
+#  start with at least two letters
+def is_first_two_letter(s):
+    if s[0:2].isalpha():
+        return True
+    else:
+        return False
+
+# 2 <= length <= 6
+def is_length(s):
+    if 2 <= len(s) <= 6:
+        return True
+    else:
+        return False
+
+# number must come at the end
+def is_middle_letter(s):
+    for c in range(len(s)):
+        if s[c].isdigit():
+            if not s[c:].isdigit():
+                return False
+    return True
+
+# the first number used cannot be a '0'
+def is_first_number(s):
+    for n in s:
+        if n.isnumeric():
+            if n != "0":
+                return True
+            else:
+                return False
+    return True
+
+# no period, spaces, or punctuation
+def is_punctuation(s):
+    disallowed_c = set(string.punctuation + " ")
+    for n in s:
+        if n in disallowed_c:
+            return False
+    return True
+
+main()
---
+++ program_twttr.py
@@ -0,0 +1,45 @@
+# solution 3: True 
+# get user input
+s = input("Input: ")
+vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+
+def main():    
+    output = ""
+    for c in s:
+        if not is_vowel(c):
+            output += c
+    print("Output:", output)
+
+def is_vowel(c):
+    if c in vowels:
+        return True
+    else:
+        return False
+
+main()
+
+
+'''
+solution 1: without for loop
+# get user input
+s = input("Input: ")
+
+output = s.replace("A", "").replace("a", "").replace("E", "").replace("e", "").replace("I", "").replace("i", "").replace("O", "").replace("o", "").replace("U", "").replace("u", "")
+
+print("Output:", output)
+'''
+
+'''
+solution 2: with for loop
+
+# get user input
+
+vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+
+output = input("Input: ")
+
+for v in vowels:
+    output = output.replace(v, "")
+
+print("Output:", output)
+'''
